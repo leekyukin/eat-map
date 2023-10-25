fea420d93fd12eab52d74929d243d2c6a4081b85
leekyukin <leekujin14@gmail.com>
2023-10-25 09:27:13 +0900

    feat: kakao map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,5 @@
-import Layout from "@/components/Layout";
-import { Inter } from "next/font/google";
-import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
+import Map from "@/components/Map";
 
 export default function Home() {
-  return (
-    <>
-      <h1>Map Index Page</h1>
-      <ul>
-        <li>
-          <Link href="/stores">맛집 목록</Link>
-        </li>
-        <li>
-          <Link href="/stores/new">맛집 생성</Link>
-        </li>
-        <li>
-          <Link href="/stores/1">맛집 상세 페이지</Link>
-        </li>
-        <li>
-          <Link href="/stores/1/edit">맛집 수정 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/login">로그인</Link>
-        </li>
-        <li>
-          <Link href="/users/my-page">마이 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/likes">찜한 맛집</Link>
-        </li>
-      </ul>
-    </>
-  );
+  return <Map />;
 }
